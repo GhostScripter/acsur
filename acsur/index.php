b5708b962b8a8aea73a3289c90dc3818366b7957
GhostScripter <git@beckmerhagen.info>
2017-02-05 22:57:32 +0100

    Test commit of single file

---
+++ acsur/index.php
@@ -0,0 +1,14 @@
+<?php
+    define("AppName", "acsure");
+    define("AppLongName", "Activity Survey");
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+	    <meta charset="UTF-8">
+    	<title><?php echo AppLongName; ?></title>
+    </head>
+    <body>
+    
+    </body>
+</html>
